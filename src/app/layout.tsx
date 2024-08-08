95fff6dbc3f78a0c5ab030e92fb3c6081a00c6df
Janghyeon <hwahyeon.dev@gmail.com>
2024-08-08 21:21:06 +0200

    style: Format code with Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Book diary",
-  description: "A web application to visualize your reading history on a calendar, with detailed book information and easy Excel upload functionality",
+  description:
+    "A web application to visualize your reading history on a calendar, with detailed book information and easy Excel upload functionality",
 };
 
 export default function RootLayout({
