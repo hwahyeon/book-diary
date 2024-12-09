1b33ba86d259086f49c5711234ad15d5db446d38
Janghyeon <hwahyeon.dev@gmail.com>
2024-12-09 21:01:55 +0100

    feat: Add Navbar

---
+++ src/data/navigation.ts
@@ -0,0 +1,18 @@
+export interface Navigation {
+  href: string;
+  label: string;
+  submenu?: { href: string; label: string }[];
+}
+
+export const navigation: Navigation[] = [
+  {
+    href: "/book",
+    label: "Books",
+    submenu: [
+      { href: "/book/", label: "Calender" },
+      { href: "/book/all", label: "View All Books" },
+    ],
+  },
+  { href: "/paper", label: "Papers" },
+  // { href: "/movie", label: "Movies" },
+];
