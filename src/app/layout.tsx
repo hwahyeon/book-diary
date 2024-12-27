e190bd3d9ff0b851366af8c53fcaa96d7d5ff74d
Janghyeon <hwahyeon.dev@gmail.com>
2024-12-27 20:04:41 +0100

    feat: Add footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className} py-20>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
