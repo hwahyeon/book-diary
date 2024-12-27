cf8769b6efbc03ca14ed92a0a301f41bef357369
Janghyeon <hwahyeon.dev@gmail.com>
2024-12-27 20:03:59 +0100

    feat: Create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="bg-background text-gray-700 text-center px-20 pt-20 pb-10">
+      <hr className="pb-5 pt-3" />
+      <p>© 2024 hwahyeon. All rights reserved.</p>
+    </footer>
+  );
+}
