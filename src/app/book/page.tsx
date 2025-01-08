9dac537d24590c5c4efe4e055cfa19abdbd38696
Janghyeon <hwahyeon.dev@gmail.com>
2025-01-08 13:25:57 +0100

    chore: Add type assertions

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -22,7 +22,7 @@ const HomePage: React.FC = () => {
       PublicationDate: book.PublicationDate,
       ISBN: book.ISBN,
       Description: book.Description,
-      BookFormat: book.BookFormat,
+      BookFormat: Number(book.BookFormat),
       Type: book.Type,
     }));
 
