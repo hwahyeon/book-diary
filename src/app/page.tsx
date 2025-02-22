dbe9f8ba3e078b3309e5aad00730838088a87755
Janghyeon <hwahyeon.dev@gmail.com>
2025-02-22 17:57:19 +0100

    feat: Simplify main page due to service expansion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,11 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import booksData from "@public/data/books.json";
-import { Book } from "../types/Book";
-import { LatestPost } from "@/components/Latest";
 import { FeaturePost } from "@/components/Feature";
 import { IntroPost } from "@/components/Intro";
 
 const HomePage: React.FC = () => {
-  const [books, setBooks] = useState<Book[]>([]);
-
-  useEffect(() => {
-    const transformedBooks: Book[] = booksData.map((book: any) => ({
-      ID: book.ID,
-      Date: book.Date,
-      Title: book.Title,
-      Writer: book.Writer,
-      PartOfSeries: book.PartOfSeries,
-      SeriesNumber: Number(book.SeriesNumber),
-      PrintLength: Number(book.PrintLength),
-      Language: book.Language,
-      Publisher: book.Publisher,
-      PublicationDate: book.PublicationDate,
-      ISBN: book.ISBN,
-      Description: book.Description,
-      BookFormat: Number(book.BookFormat),
-      Type: book.Type,
-    }));
-
-    setBooks(transformedBooks);
-  }, []);
-
   return (
     <div className="bg-background pt-16 px-4 xl:px-16 lg:px-14 md:px-8 sm:px-4 max-w-screen-xl mx-auto">
       <IntroPost />
       <FeaturePost />
-      <LatestPost posts={books} />
     </div>
   );
 };
